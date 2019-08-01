fe5e73bce74287d63cbc6f2d35ab7279861399c2
Sam Hanley <sam@sphsolutions.net>
2019-08-01 00:22:50 -0400

    Add !map invocation

--- Bots/PoGoBot.cs
+++ Bots/PoGoBot.cs
@@ -11,7 +11,6 @@ namespace PoGoChatbot.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-
             // This is a temporary hack to work around the fact that GroupMe doesn't correctly route add/join messages through OnMembersAddedAsync
 
             var addedMemberName = turnContext.Activity.getAddedGroupMeMemberName();
@@ -19,7 +18,12 @@ namespace PoGoChatbot.Bots
             if (!String.IsNullOrEmpty(addedMemberName))
             {
                 await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome, {addedMemberName}! We're always excited to have a new trainer join our community!"), cancellationToken);
-                await turnContext.SendActivityAsync(MessageFactory.Text($"I've got a few helpful resources to help you get started. To start, here's a map of the gyms where we typically raid: https://tinyurl.com/y3rddyjd"), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text($"I've got a few helpful resources to help you get started. To start, here's a map of the gyms where we typically raid: https://tinyurl.com/y3rddyjd. If you need this link later, just say \"!map\"."), cancellationToken);
+            }
+
+            if(turnContext.Activity.Text.StartsWith("!", StringComparison.Ordinal))
+            {
+                await HandleInvocationActivity(turnContext);
             }
         }
 
@@ -30,8 +34,20 @@ namespace PoGoChatbot.Bots
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome, {member.Name}! We're always excited to have a new trainer join our community!"), cancellationToken);
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"I've got a few helpful resources to help you get started. To start, here's a map of the gyms where we typically raid: https://tinyurl.com/y3rddyjd"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"I've got a few helpful resources to help you get started. To start, here's a map of the gyms where we typically raid: https://tinyurl.com/y3rddyjd. If you need this link later, just say \"!map\"."), cancellationToken);
                 }
+            }
+        }
+
+        private async Task HandleInvocationActivity(ITurnContext<IMessageActivity> turnContext)
+        {
+            switch (turnContext.Activity.Text)
+            {
+                case "!map":
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Here's a map of the gyms where we typically raid: https://tinyurl.com/y3rddyjd."));
+                    break;
+                default:
+                    break;
             }
         }
     }
